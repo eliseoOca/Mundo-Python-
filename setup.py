c41fb94a8634fcc66446771ae08badecb0bab286
Eliseo <103590535+eliseoOca@users.noreply.github.com>
2023-03-20 17:52:53 -0300

    Delete setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(
-    name="miFuncion",
-    version="1.0",
-    description="realiza sumas y comprueba si es par e impar",
-    author="Eliseo",
-    packages=["paquetes"]
-)
