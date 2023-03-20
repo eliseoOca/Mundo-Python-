55928aa09e895558eaec4f10179d2ef54f9dc2cf
Eliseo <103590535+eliseoOca@users.noreply.github.com>
2023-03-20 17:49:52 -0300

    Delete ejercicio17.py

--- ejercicio17.py
+++
@@ -1,7 +0,0 @@
-usuario=input('dame una lista de colores separados por comas: ')
-usuario=set(usuario.split(','))
-colore=[]
-for color in usuario:
-    colore.append(color)
-colore.sort()
-print(colore)
